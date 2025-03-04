39bcf0c773b54e685437786fe8985f35e6c98961
LASBENNES Lucas <lucas.lasbennes@ynov.com>
2025-03-04 17:37:59 +0100

    build(Classes and Enum): Person with birth date exercice

--- src/fr/ynov/java/medium/Person.java
+++ src/fr/ynov/java/medium/Person.java
@@ -1,5 +1,7 @@
 package fr.ynov.java.medium;
 
+import java.util.Date;
+
 public class Person {
 
     public enum nationalityEnum {
@@ -9,27 +11,32 @@ public class Person {
     }
 
     String name;
-    int age;
+    Date birthDate;
     String gender;
     float height;
     float weight;
     nationalityEnum nationality;
 
-    public Person(String name, int age, String gender, float height, float weight, nationalityEnum nationality) {
+    public Person(String name, Date birthDate, String gender, float height, float weight, nationalityEnum nationality) {
         this.name = name;
-        this.age = age;
+        this.birthDate = birthDate;
         this.gender = gender;
         this.height = height;
         this.weight = weight;
         this.nationality = nationality;
     }
 
+    public int getAge() {
+        Date currentDate = new Date();
+        return currentDate.getYear() - birthDate.getYear();
+    }
+
     public void printAttributes() {
-        System.out.println(name + " is a " + nationality + " " + gender + " of " + age + " years old, " + height + "m tall and weighs " + weight + "kg.");
+        System.out.println(name + " is a " + nationality + " " + gender + " of " + getAge() + " years old, " + height + "m tall and weighs " + weight + "kg.");
     }
 
     public static void main(String[] args) {
-        Person person1 = new Person("John", 25, "helicopter", 1.80f, 80.0f, nationalityEnum.FRENCH);
+        Person person1 = new Person("John", new Date(98, 1, 1), "helicopter", 1.80f, 80.0f, nationalityEnum.FRENCH);
         person1.printAttributes();
     }
 }
