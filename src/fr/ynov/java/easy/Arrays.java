258ae9025ea2bd8a3ecff6253d9c4a9759c176e4
LASBENNES Lucas <lucas.lasbennes@ynov.com>
2025-03-04 16:30:47 +0100

    build(arrays): Arrays exercice

---
+++ src/fr/ynov/java/easy/Arrays.java
@@ -0,0 +1,20 @@
+package fr.ynov.java.easy;
+
+public class Arrays {
+
+    public static void main(String[] args) {
+        int[] array = new int[10];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i + 1;
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            if (i == array.length - 1) {
+                System.out.println(array[i]);
+            } else {
+            System.out.print(array[i] + ", ");
+            }
+        }
+    }
+}
