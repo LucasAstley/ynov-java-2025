66fd2fb0c1d4ea0d8cea0736cc1d3f67deef7c84
LASBENNES Lucas <lucas.lasbennes@ynov.com>
2025-03-11 17:00:48 +0100

    build(Reading files): createFolder and read files of the folder

--- src/fr/ynov/java/medium/ReadFile.java
+++ src/fr/ynov/java/medium/ReadFile.java
@@ -20,7 +20,47 @@ public class ReadFile {
         }
     }
 
+    public static void createFolder(String folderPath) {
+        File theDir = new File(folderPath);
+        if (!theDir.exists()) {
+            theDir.mkdirs();
+        }
+    }
+
+    public static void createFile(String filePath) {
+        try {
+            File myObj = new File(filePath);
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (Exception e) {
+            System.out.println("An error occurred.");
+        }
+    }
+
+    public static void writeInFile(String filePath, String text) {
+        try {
+            java.io.FileWriter myWriter = new java.io.FileWriter(filePath);
+            myWriter.write(text);
+            myWriter.close();
+            System.out.println("Successfully wrote into " + filePath);
+        } catch (Exception e) {
+            System.out.println("An error occurred.");
+        }
+    }
+
     public static void main(String[] args) {
-        readFile("src/fr/ynov/java/medium/awesome_file.txt");
+        createFolder("src/fr/ynov/java/medium/awesome_folder");
+        for (int i = 0; i < 10; i++) {
+            createFile("src/fr/ynov/java/medium/awesome_folder/awesome_file" + i + ".txt");
+            String helloWorldText = "Hello World number " + i + " !";
+            writeInFile("src/fr/ynov/java/medium/awesome_folder/awesome_file" + i + ".txt", helloWorldText);
+        }
+        System.out.println("------------------------\nReading files...");
+        for (int i = 0; i < 10; i++) {
+            readFile("src/fr/ynov/java/medium/awesome_folder/awesome_file" + i + ".txt");
+        }
     }
 }
